6b6de07499de622a6186ed71df2d1543f1adb570
danteee2 <watkinsal21@outlook.com>
2023-10-17 13:06:11 +0100

    Update LS_speed_defines.lua

--- common/defines/LS_speed_defines.lua
+++ common/defines/LS_speed_defines.lua
@@ -1,4 +1,6 @@
-NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999 -- Lag days
-NDefines.NGame.LAG_DAYS_FOR_PAUSE = 999 -- pause days
-NDefines.NGame.GAME_SPEED_SECONDS = { 100.0, 0.25, 0.1, 0.05, 0.0 }
-NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 12
+NGame = { 
+LAG_DAYS_FOR_LOWER_SPEED = 999 -- Lag days
+LAG_DAYS_FOR_PAUSE = 999 -- pause days 
+GAME_SPEED_SECONDS = { 100.0, 0.25, 0.1, 0.05, 0.0 }
+COMBAT_LOG_MAX_MONTHS = 12
+}
