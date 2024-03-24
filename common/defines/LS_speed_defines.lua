d9dbcce956cd568a9a81a17cf71bd1d029b4a577
G6101 <136126849+G6101@users.noreply.github.com>
2024-03-24 16:11:35 +0100

    FO

--- common/defines/LS_speed_defines.lua
+++ common/defines/LS_speed_defines.lua
@@ -1,4 +1,4 @@
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999 -- Lag days
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 999 -- pause days
-NDefines.NGame.GAME_SPEED_SECONDS = { 0.25, 0.23, 0.19, 0.06, 0.0 } 
+NDefines.NGame.GAME_SPEED_SECONDS = { 1000.0, 0.30, 0.23, 0.19, 0.0 } 
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 12
