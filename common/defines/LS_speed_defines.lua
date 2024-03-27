d6cd0c08a5ff49d85393d74a238298ee8e22ebec
G6101 <136126849+G6101@users.noreply.github.com>
2024-03-27 16:14:43 +0100

    pu

--- common/defines/LS_speed_defines.lua
+++ common/defines/LS_speed_defines.lua
@@ -1,4 +1,10 @@
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999 -- Lag days
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 999 -- pause days
+<<<<<<< Updated upstream
 NDefines.NGame.GAME_SPEED_SECONDS = { 6000.0 , 0.12, 0.04, 0.02, 0.0 }
+=======
+
+NDefines.NGame.GAME_SPEED_SECONDS = { 6000.0 , 0.12, 0.23, 0.07, 0.0 }
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 12
